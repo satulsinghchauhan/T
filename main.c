68e0eb35838c067a57515dd30a6e069d75e84e81
satulsinghchauhan <atul.singh_bca17@gla.ac.in>
2019-07-08 15:45:11 +0530

    syntax change

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,arr[100][100],i,j;
+    printf("enter the size of array");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            arr[i][j]=i*j;
+        }
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
